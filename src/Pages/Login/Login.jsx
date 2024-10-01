640eea72a086d29a691b371de4e6ae1acc7a8a39
Hamza Sheikh <zafarhamza789@gmail.com>
2024-10-01 22:11:19 +0500

    login

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -3,8 +3,28 @@ import assets from '../../assets/assets'
 
 const Login = () => {
   return (
-    <div className='login'>
+    <div className='login-container'>
       <img className='logo' src={assets.logo_big} alt="logo" />
+
+      <form className='login-form'>
+        <h2>Sign up</h2>
+
+        <input type="text" className="form-input" placeholder='username' required />
+        <input type="email" className="form-input" placeholder='Email Address' />
+        <input type="password" className="form-input" placeholder='create password' />
+
+        <button type="submit">Sign Up</button>
+
+        <div className="login-term">
+          <input type="checkbox" />
+          <p>Agree to the terms of use & privacy policy.</p>
+        </div>
+
+        <div className="login-forgot">
+          <p className='login-toggle'>Already have an account <span>Click Here</span></p>
+        </div>
+
+      </form>
      
     </div>
   )
