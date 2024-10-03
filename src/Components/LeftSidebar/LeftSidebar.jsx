4ea0ef54b50ba4fee70220d6c8955aeb568a3661
Hamza Sheikh <zafarhamza789@gmail.com>
2024-10-03 12:03:06 +0500

    left-complete

--- src/Components/LeftSidebar/LeftSidebar.jsx
+++ src/Components/LeftSidebar/LeftSidebar.jsx
@@ -4,13 +4,30 @@ import assets from '../../assets/assets/'
 const LeftSidebar = () => {
   return (
     <div className='left-side'>
-        <div className="left-side-top">
-            <div className="left-side-nav">
-                <img src={assets.logo} alt="logo" className='logo' />
-            </div>
-
+      <div className="left-side-top">
+        <div className="left-side-nav">
+          <img src={assets.logo} alt="logo" className='logo' />
+          <div className="menu">
+            <img src={assets.menu_icon} alt="" />
+          </div>
         </div>
-      
+        <div className="left-side-search">
+          <img src={assets.search_icon} alt="" />
+          <input type="text" placeholder='Search here' />
+        </div>
+      </div>
+
+      <div className="left-side-list">
+        {Array(10).fill('').map((item, index) => (
+          <div key={index} className="friends">
+            <img src={assets.profile_img} alt="" />
+            <div>
+              <p>Hamza Sheikh</p>
+              <span>Hello, How are you?</span>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
